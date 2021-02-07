d54b0168fc32cb3ecdf6427bf8e2e3e795c5d3f8
Hasan Siam <hasansiam4545@gmail.com>
2021-02-08 01:48:00 +0600

    updated

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const displaySearch = () => {
+const displaySearch = async () => {
     const searchText = document.getElementById('search_field').value;
     const url = `https://api.lyrics.ovh/suggest/${searchText}`;
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displaySongs(data.data))
+    const res = await fetch(url)
+    const data = await res.json()
+    displaySongs(data.data)
 }
 
 const displaySongs = songs => {
+    const songContainer = document.getElementById('song_container');
+    songContainer.innerHTML = '';
     songs.forEach(song => {
-        console.log(song)
-        const songContainer = document.getElementById('song_container');
         const songDiv = document.createElement('div');
         songDiv.className = "single-result row align-items-center my-3 p-3";
         songDiv.innerHTML = `
@@ -21,10 +21,23 @@ const displaySongs = songs => {
                          </audio> 
                     </div>
                     <div class="col-md-3 text-md-right text-center">
-                        <button class="btn btn-success">Get Lyrics</button>
+                        <button onclick = "getLyrics('${song.artist.name}','${song.title}')" class="btn btn-success">Get Lyrics</button>
                     </div>
         `
 
         songContainer.appendChild(songDiv)
     });
+}
+
+const getLyrics = async (artist, title) => {
+    const url = `https://api.lyrics.ovh/v1/${artist}/${title}`;
+    const res = await fetch(url)
+    const data = await res.json()
+    displayLyrics(data.lyrics)
+}
+
+const displayLyrics = lyrics => {
+
+    const lyricsDiv = document.getElementById('lyrics');
+    lyricsDiv.innerText = lyrics
 }
